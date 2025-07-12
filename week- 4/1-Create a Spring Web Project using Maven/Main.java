d5580e54930455ead4775e18b89b7f5d20c07ff9
Banumathi B S <144659335+banu1609@users.noreply.github.com>
2025-07-12 22:09:57 +0530

    Add files via upload

---
+++ week- 4/1-Create a Spring Web Project using Maven/Main.java
@@ -0,0 +1,6 @@
+public class SpringLearnApplication {
+    public static void main(String[] args) {
+        System.out.println("SpringLearnApplication started!");
+        SpringApplication.run(SpringLearnApplication.class, args);
+    }
+}
